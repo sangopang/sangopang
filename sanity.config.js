2d0749bb1074413f05b35f6dd1f957483185b86f
kp1153 <kamta_tiwari@yahoo.com>
2025-11-16 15:12:02 +0530

    force deploy

--- sanity.config.js
+++ sanity.config.js
@@ -7,7 +7,7 @@
 import { visionTool } from "@sanity/vision";
 import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
-import { cloudinaryImageAsset } from "sanity-plugin-cloudinary";
+import { cloudinarySchemaPlugin } from "sanity-plugin-cloudinary";
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import { apiVersion, dataset, projectId } from "./sanity/env";
