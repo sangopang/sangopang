d9d828908c7ac5716d6263f28647f71869b7635d
sangopang <sangopangm@gmail.com>
2025-11-28 15:01:56 +0530

    force deploy

--- sanity.config.js
+++ sanity.config.js
@@ -1,28 +1,20 @@
-"use client";
-
-/**
- * This configuration is used to for the Sanity Studio that's mounted on the `\app\studio\[[...tool]]\page.jsx` route
- */
-
-import { visionTool } from "@sanity/vision";
-import { defineConfig } from "sanity";
-import { structureTool } from "sanity/structure";
-
-// Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
-import { apiVersion, dataset, projectId } from "./sanity/env";
-import { schema } from "./sanity/schemaTypes";
-import { structure } from "./sanity/structure";
+import { defineConfig } from 'sanity'
+import { visionTool } from '@sanity/vision'
+import { schemaTypes } from './schemas'
 
 export default defineConfig({
-  basePath: "/studio",
-  projectId,
-  dataset,
-  // Add and edit the content schema in the './sanity/schemaTypes' folder
-  schema,
+  name: 'default',
+  title: 'Sangopang Studio',
+
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+
   plugins: [
-    structureTool({ structure }),
-    // Vision is for querying with GROQ from inside the Studio
-    // https://www.sanity.io/docs/the-vision-plugin
-    visionTool({ defaultApiVersion: apiVersion }),
+    visionTool(), // Sanity Vision Tool (works with v3)
   ],
-});
+
+  schema: {
+    types: schemaTypes,
+  },
+})
+
